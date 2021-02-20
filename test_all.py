0d7df6fa60a3563e5b72749fb2e30aed78151b50
Oleg Panichev <o_pan@mail.ru>
2021-02-20 09:47:54 +0300

    Tests finished

--- test_all.py
+++ test_all.py
@@ -64,8 +64,123 @@ def test_create_str_wrong_date(date):
         date = Date(date)
 
 
-@pytest.mark.parametrize("date", [
+@pytest.mark.parametrize(("date", "int_day", "int_month", "int_year"), [
+    ("01.01.0001", 1, 1, 1),
+    ("29.02.0020", 29, 2, 20),
+    ("01.01.0000", 1, 1, 0),
+    ("13.12.3000", 13, 12, 3000)])
+def test__str__(date, int_day, int_month, int_year):
+    dt = Date(int_day, int_month, int_year)
+    assert date == dt.__str__()
 
+
+@pytest.mark.parametrize(("date", "int_day", "int_month", "int_year"), [
+    ("Date(1, 1, 1)", 1, 1, 1),
+    ("Date(29, 2, 20)", 29, 2, 20),
+    ("Date(1, 1, 0)", 1, 1, 0),
+    ("Date(13, 12, 3000)", 13, 12, 3000)])
+def test__repr__(date, int_day, int_month, int_year):
+    dt = Date(int_day, int_month, int_year)
+    assert date == dt.__repr__()
+
+
+@pytest.mark.parametrize(("year", "result"), [
+    (2020, True),
+    (2021, False),
+    (2000, True),
+    (1900, False)])
+def test__leap__fun(year, result):
+    assert result == Date.is_leap_year(year)
+
+
+@pytest.mark.parametrize(("month", "year", "result"), [
+    (1, 2020, 31),
+    (2, 2020, 29),
+    (2, 2021, 28),
+    (12, 2020, 31),
+    (2, 1900, 28),
+    (3, 22, 31),
+    (4, 33, 30),
+    (5, 44, 31),
+    (6, 55, 30),
+    (7, 166, 31),
+    (8, 177, 31),
+    (9, 1188, 30),
+    (10, 2220, 31),
+    (11, 3120, 30)])
+def test__max_day_fun(month, year, result):
+    assert result == Date.get_max_day(month, year)
+
+
+@pytest.mark.parametrize(("date1", "date2", "result"), [
+    ("01.12.2020", "01.12.2021", 365),
+    ("01.12.2021", "01.12.2020", -365),
+    ("01.12.2019", "01.12.2020", 366),
+    ("01.12.2020", "01.12.2019", -366),
+    ("01.12.2010", "01.12.2020", 3653),
+    ("01.12.2020", "01.12.2010", -3653),
+    ("31.07.2019", "31.08.2019", 31),
+    ("31.08.2019", "31.07.2019", -31),
+    ("10.07.2019", "11.07.2019", 1),
+    ("31.07.2019", "31.07.2019", 0)
 ])
-def test__str__fun(date):
-    a = Date.__
+def test__sub__fun(date1, date2, result):
+    dt1 = Date(date1)
+    dt2 = Date(date2)
+    assert result == dt2.__sub__(dt1)
+
+
+@pytest.mark.parametrize(
+    ("date", "delta_day", "delta_month", "delta_year", "delta_res", "res"),
+    [("01.01.0001", 0, 0, 1, "01.01.0002", "01.01.0001"),
+     ("01.01.2020", 0, 12, 0, "01.01.2021", "01.01.2020"),
+     ("01.01.2020", 365, 0, 0, "31.12.2020", "01.01.2020"),
+     ("01.01.2020", 365, 10, 10, "01.11.2031", "01.01.2020")
+     ])
+def test__add__(date, delta_day, delta_month, delta_year, delta_res, res):
+    dt = Date(date)
+    td = TimeDelta(delta_day, delta_month, delta_year)
+    assert Date(delta_res).__str__() == dt.__add__(td).__str__()
+    assert dt.__str__() == res
+
+
+@pytest.mark.parametrize(("date", "delta_day", "delta_month", "delta_year"),
+                         [("01.01.2020", 365, 0, 1000),
+                          ("01.01.1", 365, 10, 2999)])
+def test__add_wrong(date, delta_day, delta_month, delta_year):
+    with pytest.raises(ValueError):
+        dt = Date(date)
+        td = TimeDelta(delta_day, delta_month, delta_year)
+        dt.__add__(td)
+
+
+@pytest.mark.parametrize(("date", "trash"),
+                         [("01.01.2020", 365),
+                          ("01.01.1", "hey, get me a beer")])
+def test__add_wrong_class(date, trash):
+    with pytest.raises(ValueError):
+        dt = Date(date)
+        dt.__add__(trash)
+
+
+@pytest.mark.parametrize(
+    ("date", "delta_day", "delta_month", "delta_year", "res"),
+    [("01.01.0001", 0, 0, 1, "01.01.0002"),
+     ("01.01.2020", 0, 12, 0, "01.01.2021"),
+     ("01.01.2020", 365, 0, 0, "31.12.2020"),
+     ("01.01.2020", 365, 10, 10, "01.11.2031")
+     ])
+def test__add__(date, delta_day, delta_month, delta_year, res):
+    dt = Date(date)
+    td = TimeDelta(delta_day, delta_month, delta_year)
+    dt.__iadd__(td)
+    assert res == dt.__str__()
+
+
+@pytest.mark.parametrize(("date", "trash"),
+                         [("01.01.2020", 365),
+                          ("01.01.1", "hey, give me another beer")])
+def test__iadd_wrong_class(date, trash):
+    with pytest.raises(ValueError):
+        dt = Date(date)
+        dt.__iadd__(trash)
