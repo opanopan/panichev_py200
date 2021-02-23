6aca67f7c167a71ae207bc3fbcabc9f3a2b6d15a
Oleg Panichev <o_pan@mail.ru>
2021-02-23 14:21:07 +0300

    setup + travis ci

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+__pckg__ = "date"
+__dpckg__ = __pckg__.replace("-", "_")
+__version__ = "0.1"
+
+setup(
+    name=__pckg__,
+    version=__version__,
+    description="Date package",
+    author="Panichev Oleg",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    extras_require={
+        "tests": [
+            "pytest = 6,2,2",
+            "pytest-cov = 2.11.1",
+        ]
+    }
+)
